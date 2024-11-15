a7e56736acbb082815019ca213477b6a102a2bf1
Tirumala Raju Mahali <103620129+Tirumala02@users.noreply.github.com>
2024-11-15 20:18:52 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '₹';
     const delivery_fee = 10;
-    const backendUrl = "https://xchnagetechsecom-backend.onrender.com"
+    const backendUrl = "https://xchnagetechsecom-back.onrender.com"
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
